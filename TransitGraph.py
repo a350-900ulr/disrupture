e361d84f1083351fe41d7d06ef758d3a936be5b1
will amminger <e12229199@student.tuwien.ac.at>
2024-09-26 14:29:04 +0200

    function for list of lines during journey

--- TransitGraph.py
+++ TransitGraph.py
@@ -1,17 +1,27 @@
 import networkx as nx
 from os import listdir
 from re import match
+from copy import deepcopy
 
 
 class TransitGraph(nx.Graph):
-	# between stations
+	# Assumptions: all stations are bidirectional
+	
+	# Travel times between stations, assuming that all intervals are the same.
 	travel_times = {
-		'tram': 2,
-		'trolley': 2,
+		'tram': 1.5,
+		'trolley': 1.75,
 		'metro': 2,
-		'commuter': 2
+		'commuter': 3
 	}
 	
+	# Typical interval during peak times
+	wait_times = {
+		'tram': 6,
+		'trolley': 7.5,
+		'metro': 3.5,
+		'commuter': 30,
+	}
 	
 	def load_default_graph(self, line_folder: str = 'lines'):
 		
@@ -24,46 +34,97 @@ class TransitGraph(nx.Graph):
 					stations.append(line.strip())
 			self.add_lines(line_name, stations)
 		
-	def add_lines(self, line_name: str, stations: list[str], custom_type: str = None):
+	def add_lines(self,
+		line_name: str,
+		stations: list[str],
+		custom_type: str = None,
+		verbose: bool = False
+	) -> None:
 		if custom_type is None:
 			line_type = self.detect_line_type(line_name)
 		else:
 			line_type = custom_type
 	
-		for index, station in enumerate(stations[:-1]):
-			# TODO: if line already exist, append line to list of lines
-			# also always make it a list
-			self.add_edge(
-				station,
-				stations[index + 1],
-				line=line_name,
-				type=line_type,
-				travel_time=self.travel_times[line_type]
-			)
+		for index, station1 in enumerate(stations[:-1]):
+			station2 = stations[index + 1]
+			
+			if self.has_edge(station1, station2):
+				self[station1][station2]['lines'].add(line_name)
+				'''
+				If the line is of a different type than the existing value, convert it to the
+				type with the shortest travel time. This is based on the same assumption that
+				longer distance lines accommodate the distances of shorter distance lines when
+				going into the city as it was most likely built for the shorter distance line.
+				'''
+				if line_type != self[station1][station2]['type']:
+					if verbose:
+						print(
+							'\nMultiple lines of different type on the same edge detected.'
+							f'\n\tExisting edge: {station1} - {station2}'
+							f'\n\tExisting type: {self[station1][station2]['type']}'
+							f'\n\tProposed type: {line_type}'
+						)
+					# if new line type is faster than previous
+					if self.travel_times[line_type] < self[station1][station2]['travel_time']:
+						self[station1][station2]['type'] = line_type
+						self[station1][station2]['travel_time'] = self.travel_times[line_type]
+			else:
+				self.add_edge(
+					station1,
+					station2,
+					lines={line_name},  # encapsulate in a set in case of shared lines
+					type=line_type,
+					travel_time=self.travel_times[line_type],
+				)
 	
 	def detect_line_type(self, line_name: str) -> str:
-		if match('^\d{1,2}$', line_name):
+		if match('^\\d{1,2}$', line_name) or line_name in ['D', 'O']:
 			line_type = 'tram'
-		elif match('^U\d$', line_name):
+		elif match('^U\\d$', line_name):
 			line_type = 'metro'
-		elif match('^S\d{1,2}$', line_name):
-			line_type = 'commuter'
-		elif line_name == 'Stammstrecke':  # manually added for testing, to be split later
+		elif match('^S\\d{1,2}$', line_name) or line_name == 'Stammstrecke':
 			line_type = 'commuter'
 		else:
 			raise ValueError(f'Could not automatically determine type of {line_name}')
 		return line_type
 
-	def fastest_path(self, source, target, paths_before_transfers: int = 1):
-		# find top k paths
+	def fastest_path(self, source, target, paths_before_transfers: int = 6):
+		"""
+		Finds the fastest path between a source & target station.
+		:param source:
+		:param target:
+		:param paths_before_transfers:
+		:return:
+		"""
+		
 		path_generator = nx.shortest_simple_paths(self, source, target, weight='travel_time')
-		top_paths = []
+		
+		# get a certain amount of journeys using only travel_time weights
+		top_paths: list[list[str]] = []
 		for index, path in enumerate(path_generator):
 			if index >= paths_before_transfers:
 				break
 			top_paths.append(path)
 		
-		# add transfer weights
+		# calculate transit times & get list of possible lines throughout the route
+		total_times = []
+		
+		for candidate_path in top_paths:
+			
+			lines: list[set[str]] = []
+			current_time = 0
+			
+			# get initial segment line & transit times
+			for index, station1 in enumerate(candidate_path[:-1]):
+				station2 = candidate_path[index + 1]
+				
+				lines.append(self[station1][station2]['lines'])
+				current_time += self[station1][station2]['travel_time']
+			
+			# get line series & add transfer times
+			
+			
+				
 		
 		# take top (mark if its ever different?)
 
@@ -71,5 +132,6 @@ class TransitGraph(nx.Graph):
 
 net = TransitGraph()
 net.load_default_graph()
-net.fastest_path('Schönbrunn', 'Stephansplatz')
+test = net.fastest_path('Schönbrunn', 'Stephansplatz')
 #arr = listdir('lines')
+
--- testing.py
+++ testing.py
@@ -1,5 +1,8 @@
+# scratchpad file
+
 import networkx as nx
 from TransitGraph import TransitGraph as tg
+from copy import deepcopy
 
 net = tg()
 
@@ -93,3 +96,110 @@ print(f"Fastest route from {source} to {target}:", path)
 print(f"Total travel time: {total_time} minutes")
 
 
+
+
+
+
+
+from TransitGraph import TransitGraph as tg
+
+net = tg()
+
+net.add_edge('test', 'test2', line=['U4'])
+
+
+net.has_edge('test', 'test2')
+net.has_edge('test2', 'test')
+
+
+print(net['test']['test2'])
+net['test']['test2']['line'].append('U3')
+print(net['test']['test2'])
+
+
+
+
+from copy import deepcopy
+def minimize_changes(possible_lines: list[set[str]]) -> list[str]:
+	
+	def strip_extra_lines(possible_lines_inner) -> list[set[str]]:
+		# add first segment to total lines
+		total_lines = [possible_lines_inner[0]]
+		# go through the rest of the journey
+		for next_lines in possible_lines_inner[1:]:
+			# if the next segment has different lines
+			if total_lines[-1] != next_lines:
+				# 1st create a duplicate of the current lines for reconsideration
+				total_lines.append(deepcopy(total_lines[-1]))
+				
+				# create a duplicate of that for iteration
+				considered_lines = deepcopy(total_lines[-1])
+				
+				# delete any that do not appear in the next segment
+				for line in considered_lines:
+					if line not in next_lines:
+						total_lines[-1].remove(line)
+				
+				# if there are no continued lines, add all lines from the next segment
+				if len(total_lines[-1]) == 0:
+					total_lines[-1] = next_lines
+			
+		return total_lines
+	
+	input_path = strip_extra_lines(possible_lines)
+	input_path.reverse()
+	input_path = strip_extra_lines(input_path)
+	input_path.reverse()
+	
+	condensed_path = []
+	for line in input_path:
+		#print(condensed_path)
+		assert len(line) == 1, f'this should not happen but just incase'
+		# extra single line from set
+		next_line = next(iter(line))
+		# add if not already there
+	
+		if len(condensed_path) == 0:
+			condensed_path.append(next_line)
+		else:
+			if condensed_path[-1] != next_line:
+				condensed_path.append(next_line)
+		
+	return condensed_path
+	
+	
+
+input_list = [
+	{'2', '10', '60'},
+	{'2', '10', '60'},
+	{'2', '52', '60'},
+	{'2', '52', '60'},
+	{'2', '6', '18'},
+	{'2', '6', '18'},
+	{'2', '18'},
+	{'2', '18', '4'},
+	{'2', '18'},
+	{'18', '1'},
+	{'18', '1'},
+	{'1'}
+]
+
+output = minimize_changes(input_list)
+print(output)
+
+
+input_list = [
+	{'10', '60'},
+	{'10', '60'},
+	{'52', '60'},
+	{'52', '60'},
+	{'6', '18'},
+	{'6', '18'},
+	{'18'},
+	{'18', '4'},
+	{'18'},
+	{'18', '1'},
+	{'18', '1'},
+	{'1'}
+]
+print(minimize_changes(input_list))
