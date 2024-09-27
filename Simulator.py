285f1305483db744f6ac2265107955facbb75d83
will amminger <e12229199@student.tuwien.ac.at>
2024-09-27 14:37:41 +0200

    simulations with stats generated

--- Simulator.py
+++ Simulator.py
@@ -1,6 +1,26 @@
 from TransitGraph import TransitGraph as TG
+from random import sample
 
-# either calculate only by number of affected routes or all?
-def get_disruption_stats(station: str, simulated_runs: int = 1_000):
-	for i in range(simulated_runs):
-		...
+
+class Simulator:
+	def __init__(self, journey_count: int = 1_000):
+		self.journey_count = journey_count
+		self.net = TG()
+		self.stations = list(self.net.nodes)
+		self.journeys = list(dict())
+		
+	def generate_journeys(self):
+		for i in range(self.journey_count):
+			origin, target = sample(self.stations, k=2)
+			lines, time, stations = self.net.fastest_path(origin, target)
+			self.journeys.append({
+				'origin': origin,
+				'target': target,
+				'lines': lines,
+				'time': time,
+				'stations': stations
+			})
+			
+	def get_disruption_stats(station: str, simulated_runs: int = 1_000):
+		for i in range(simulated_runs):
+			...
